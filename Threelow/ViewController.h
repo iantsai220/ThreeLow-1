5e755d96db2919c244d7792b8a54bced83c5eae8
iantsai <iantsai86@gmail.com>
2015-05-12 19:59:30 -0700

    initial commit

---
+++ Threelow/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Threelow
+//
+//  Created by Ian Tsai on 2015-05-12.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)RollDiceButtonPressed:(UIButton *)sender;
+
+
+- (IBAction)resetDiceButtonPressed:(UIButton *)sender;
+
+
+@end
+
---
+++ Threelow/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Threelow
+//
+//  Created by Ian Tsai on 2015-05-12.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)RollDiceButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)resetDiceButtonPressed:(UIButton *)sender {
+}
+@end
